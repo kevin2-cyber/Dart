8c3eedeb752a98fe024b1fa7715b00835020b330
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-10-25 05:56:15 -0700

    programming in dart

--- practice1/main.dart
+++ practice1/main.dart
@@ -1,4 +1,9 @@
 void main(List<String> args) {
   var car = 'BMW';
-  print('This is my new ${car}');
+  int sum2 = sum(4, 6);
+  print('This is my new ${car}, ${sum2}');
+}
+
+int sum(int num1, int num2) {
+  return num1 + num2;
 }
