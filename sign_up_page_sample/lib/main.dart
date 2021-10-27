ebed909dd94a086f1eea7f3c630e16e0c84d730f
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-10-27 09:11:42 -0700

    added a background image

--- sign_up_page_sample/lib/main.dart
+++ sign_up_page_sample/lib/main.dart
@@ -12,57 +12,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sign Up',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+      //  appBar: AppBar(
+       //   title: const Center(
+        //    child: Text('Register with us'),
+       //   ),
+      //    backgroundColor: Colors.lightBlueAccent,
+     //   ),
+        body: Container(
+          constraints: const BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/snowman.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Register with us'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key, required String title}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Container();
   }
 }
+
