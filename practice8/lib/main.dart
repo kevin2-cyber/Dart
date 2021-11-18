08d433ed105e05fba4b58da2b6c2150fbea6563a
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-11-17 19:34:17 -0800

    void

--- practice8/lib/main.dart
+++ practice8/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  void playSound(int sound){
+  void playSound(int sound) {
     final player = AudioCache();
     player.play('note$sound.wav');
   }
@@ -17,19 +17,22 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Column(
-          children: [
-            TextButton(
-              onPressed: (){
-                playSound(1);
-              },
-              child: const Text('Click Me'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.red),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                child: const Text('Click Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
               ),
             ),
             Expanded(
               child: TextButton(
-                onPressed: (){
+                onPressed: () {
                   playSound(2);
                 },
                 child: const Text('Click Me'),
@@ -38,49 +41,59 @@ class MyApp extends StatelessWidget {
                 ),
               ),
             ),
-            TextButton(
-              onPressed: (){
-                playSound(3);
-              },
-              child: const Text('Click Me'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.yellow),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                child: const Text('Click Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                ),
               ),
             ),
-            TextButton(
-              onPressed: (){
-                playSound(4);
-              },
-              child: const Text('Click Me'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.green),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                child: const Text('Click Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                ),
               ),
             ),
-            TextButton(
-              onPressed: (){
-                playSound(5);
-              },
-              child: const Text('Click Me'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blue),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                child: const Text('Click Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
               ),
             ),
-            TextButton(
-              onPressed: (){
-                playSound(6);
-              },
-              child: const Text('Click Me'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.indigo),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                child: const Text('Click Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.indigo),
+                ),
               ),
             ),
-            TextButton(
-              onPressed: (){
-                playSound(7);
-              },
-              child: const Text('Click Me'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.purple),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                child: const Text('Click Me'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.purple),
+                ),
               ),
             ),
           ],
@@ -89,4 +102,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
