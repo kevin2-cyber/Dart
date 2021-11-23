dc8d8e4c7bf670832e29548bd21b95f2e9382382
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-11-22 17:45:42 -0800

    OOP in Dart

--- practice1/main.dart
+++ practice1/main.dart
@@ -1,7 +1,23 @@
+
+class Car{
+
+  int numberOfSeats = 5;
+  void drive(){
+    print('I am driving');
+  }
+
+}
+
+
+
+
 void main(List<String> args) {
   var car = 'BMW';
   int sum2 = sum(4, 6);
   print('This is my new ${car}, ${sum2}');
+
+  Car myCar = Car();
+  myCar.drive();
 }
 
 int sum(int num1, int num2) {
