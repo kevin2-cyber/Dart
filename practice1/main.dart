1a4403fade95beed15b131630747659668b40c98
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2021-12-29 16:57:21 +0000

    learnng dart

--- practice1/main.dart
+++ practice1/main.dart
@@ -1,16 +1,18 @@
 
 // import a method from the dart:io library
-import 'dart:io';
+//import 'dart:io';
 
 // Define a function.
-void printInteger(String aNumber) {
+/*void printInteger(String aNumber) {
   print('The name is $aNumber.'); // Print to console.
-}
+}*/
 
 // This is where the app starts executing.
 void main(List<String> args) {
   //var number = 42; // Declare and initialize a variable.
   //printInteger(number); // Call a function.
-  String? name = stdin.readLineSync(); // accepting input from the user and make it nullable
-  printInteger(name!); // call a function and add a null check
+  //String? name = stdin.readLineSync(); // accepting input from the user and make it nullable
+  int name_him = 2; // snake_case variable in dart
+  print(name_him);
+  //printInteger(name!); // call a function and add a null check
 }
