f27dbc8f4e9cbd4b90e57b2568035ebf26dac59e
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2021-12-30 14:49:01 +0000

    dart

--- practice1/main.dart
+++ practice1/main.dart
@@ -8,11 +8,22 @@
 }*/
 
 // This is where the app starts executing.
-void main(List<String> args) {
+/*void main(List<String> args) {
   //var number = 42; // Declare and initialize a variable.
   //printInteger(number); // Call a function.
   //String? name = stdin.readLineSync(); // accepting input from the user and make it nullable
   int name_him = 2; // snake_case variable in dart
   print(name_him);
   //printInteger(name!); // call a function and add a null check
-}
+}*/
+
+/*class Car {
+  int? car;
+
+}*/
+
+// values can't be null unless you say they can in null safety
+
+void main(List<String> args) {}
+
+
